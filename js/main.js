985eecd4577ae8e18b3aa466698c83fdda70a0f6
Yevhen <eugenepes@gmail.com>
2019-10-27 17:43:14 +0200

    zadanie7

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+let start = document.querySelector('#start'),
+    budget = document.querySelector('.budget-value'),
+    day = document.querySelector('.daybudget-value'),
+    level = document.querySelector('.level-value'),
+    expenses = document.querySelector('.expenses-value'),
+    income = document.querySelector('.income-value'),
+    month = document.querySelector('.monthsaving-value'),
+    year = document.querySelector('.yearsavings-value'),
+
+
+    expensesItem = document.getElementsByClassName('expenses-item'),
+    expensesBtn = document.getElementsByTagName('button') [0],
+    optionalExpensesBtn = document.getElementsByTagName('button') [1],
+    countBtn = document.getElementsByTagName('button') [2],
+    
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    checkbox = document.querySelector('#savings'),
+    sum = document.querySelector('.choose-sum'),
+    percent = document.querySelector('.choose-percent'),
+    year1 = document.querySelector('.year-value'),
+    month1 = document.querySelector('.month-value'),
+    day1 = document.querySelector('.day-value');
+
+
